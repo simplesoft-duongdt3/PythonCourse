2b8d32fed4aea574f5cca32df1027ecc9b5ad3fc
thanhduong.doan <thanhduong.doan@nhnent.com>
2021-01-25 15:44:57 +0700

    add parameters inserts

--- src/database_sqlite/sqlite_samples_insert_update_delete.py
+++ src/database_sqlite/sqlite_samples_insert_update_delete.py
@@ -1,8 +1,16 @@
 import sqlite3
 
-conn = sqlite3.connect('chinook.db', isolation_level= None) # auto comit isolation_level= None
+#conn = sqlite3.connect('chinook.db', isolation_level= None) # auto comit isolation_level= None
+conn = sqlite3.connect('chinook.db')
 print ("Opened database successfully", conn)
-conn.execute("INSERT INTO genres VALUES (26,'Opera 2')")
-# conn.commit()
+conn.execute("INSERT INTO genres VALUES (125, 'genre 125')")
+
+conn.execute("INSERT INTO genres VALUES (?,?)", (126, "genre 126"))
+
+conn.execute("INSERT INTO genres VALUES (?,?)", [1278, "genre 127"])
+
+conn.execute("INSERT INTO genres VALUES (:id, :name)", { "id": 128, "name": "genre 128"})
+
+conn.commit()
 
 conn.close()
